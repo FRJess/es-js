ff0ed28df8084a192baafce1ab438e36b175b423
FR_Jess <fr_jess@icloud.com>
2022-10-11 09:49:42 +0200

    [add] maths

--- esempio-2/js/script.js
+++ esempio-2/js/script.js
@@ -4,3 +4,16 @@ const prezzoProdotto = 8;
 const iva = 0.22;
 const sconto = 0.5; 
 const prodottiRichiesti = parseInt(prompt('Quanti ne vuoi acquistare?'));
+
+let prezzoIntero= prodottiRichiesti * prezzoProdotto;
+let ivaCalcolata = prezzoIntero * iva;
+let prezzoConIva = ivaCalcolata + prezzoIntero;
+let prezzoScontato = prezzoConIva - prezzoConIva * sconto;
+
+document.getElementById("risultato").innerHTML=`
+Il prodotto selezionato costa ${prezzoProdotto}€ <br>
+Tu ne hai acquistati ${prodottiRichiesti}: <br>
+il tuo prezzo intero (compreso di iva) è ${prezzoConIva}€ <br>
+ma noi qui siamo buoni quindi ti regaliamo il ${sconto * 100}% di sconto, di conseguenza il tuo prezzo finale è: <br>
+<strong>${prezzoScontato}€</strong>.
+ `
